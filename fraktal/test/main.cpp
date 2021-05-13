44f5e07023a38bdf21c764bbca09ddf417a06d69
emil <varadi.emil3@gmail.com>
2021-05-13 19:15:38 +0300

    added mandelbrot implementation, made by attila

--- fraktal/test/main.cpp
+++ fraktal/test/main.cpp
@@ -1,21 +1,39 @@
 #include "../src/fern/fern.hpp"
+#include "../src/mandelbrot/mandelbrot.hpp"
 #include "../src/utility/matrix2d.hpp"
 
 using namespace std;
 int main() {
     Matrix2d<double> matrix(5, 5);
     matrix.print();
-    std::cout << matrix(2, 3);
+    std::cout << matrix(2, 3) << std::endl;
     matrix.writeToFile("matrix2dtest.bin");
 
-    Fern newFern;
-    newFern.generateFractal(150, 50);
-    for (int i = 0; i < newFern.nX; i++) {
-        for (int j = 0; j < newFern.nY; j++) {
-            if (newFern.densityMap->operator()(i, j) == 0) {
+    std::cout << "Fern: " << std::endl;
+
+    Fern fern;
+    fern.generateFractal(150, 50);
+    for (int i = 0; i < fern.nX; i++) {
+        for (int j = 0; j < fern.nY; j++) {
+            if (fern.densityMap->operator()(i, j) == 0) {
                 std::cout << " ";
             } else {
                 std::cout << "*";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Mandelbrot: " << std::endl;
+
+    Mandelbrot mandelbrot;
+    mandelbrot.generateFractal(150, 50);
+    for (int i = 0; i < mandelbrot.nY; i++) {
+        for (int j = 0; j < mandelbrot.nX; j++) {
+            if (mandelbrot.densityMap->operator()(i, j) >= 100) {
+                std::cout << '*';
+            } else {
+                std::cout << ' ';
             }
         }
         std::cout << std::endl;
